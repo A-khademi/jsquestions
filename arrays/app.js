09f99cdadb1faee60abe21930711b1adc272cd5d
Ali <118660343+A-khademi@users.noreply.github.com>
2023-08-13 18:59:57 +0330

    Update app.js

--- arrays/app.js
+++ arrays/app.js
@@ -6,3 +6,19 @@ function myForEach(array, callback) {
 }
 function items(item, index, array) {}
 myForEach(names, item);
+
+const ages = [32, 33, 16, 40, 13, 20];
+function myFilter(array, callback) {
+  const filteredArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      filteredArray.push(array[i]);
+    }
+  }
+  return filteredArray;
+}
+
+function checkAge(age) {
+  return age > 20;
+}
+myFilter(ages, checkAge);
