9cc9dd570a8b28ea18f8d50083735a2c949e8e41
Ali <118660343+A-khademi@users.noreply.github.com>
2023-08-13 13:49:52 +0330

    Update app.js

--- arrays/app.js
+++ arrays/app.js
@@ -2,7 +2,6 @@ const array = ["hasan", "omid", "neda", "ali"];
 function myForEach(array, callBack) {
   for (let i = 0; i < array.length; i++) {
     const result = callBack(array[i], i, array);
-    console.log(result);
   }
 }
 function printItem(item, index, array) {
