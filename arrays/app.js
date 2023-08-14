763ced52a890d9e673f966fcf8bf8c3528056018
Ali <118660343+A-khademi@users.noreply.github.com>
2023-08-14 13:59:53 +0330

    Update app.js

--- arrays/app.js
+++ arrays/app.js
@@ -1,13 +1,12 @@
-const names = ["hasan", "omid", "neda", "ali"];
-function myForEach(array, callback) {
-  for (let i = 0; i < array.length; i++) {
-    callback(array[i], i, array);
+
+function myForEach(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i], i, arr);
   }
 }
 function items(item, index, array) {}
 myForEach(names, item);
 /////////////////////////////////////
-const ages = [32, 33, 16, 40, 13, 20];
 function myFilter(array, callback) {
   const filteredArray = [];
   for (let i = 0; i < array.length; i++) {
@@ -19,11 +18,10 @@ function myFilter(array, callback) {
 }
 
 function checkAge(age) {
-  return age > 20;
+  return 
 }
 myFilter(ages, checkAge);
 ////////////////////////////////////////
-const numbers = [65, 44, 12, 4];
 function myMap(arr, callback) {
   const newArr = [];
   for (let i = 0; i < arr.length; i++) {
@@ -32,7 +30,7 @@ function myMap(arr, callback) {
   return newArr;
 }
 function mapReplac(num) {
-  return num * 10;
+  return ;
 }
 const newArr = myMap(numbers, mapReplac);
 ///////////////////////////////////////////
