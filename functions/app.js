922200ac9971d986172968ad2ea01c4f72f2d422
Ali <118660343+A-khademi@users.noreply.github.com>
2023-08-26 18:03:36 +0330

    Update app.js

--- functions/app.js
+++ functions/app.js
@@ -16,9 +16,75 @@ function calculate(x) {
       };
     };
   }
+  ///////////////////////////////////////////
+function sumFromTo(start, end) {
+  if (start === end) {
+    return start;
+  }
   
-  console.log(calculate(2)(1)('+')); // 5
-  console.log(calculate(2)(2)('-')); // -1
-  console.log(calculate(2)(3)('*')); // 6
-  console.log(calculate(2)(2)('/')); // 0.666...
-  
+  return start + sumFromTo(start + 1, end);
+}
+////////////////////////////////////////////
+function callOnceAfter(func, time) {
+  let isScheduled = false;
+  let lastCallTime = null;
+
+  function scheduleCall() {
+    isScheduled = true;
+    setTimeout(function () {
+      if (isScheduled && Date.now() - lastCallTime >= time * 1000) {
+        func();
+        isScheduled = false;
+      }
+    }, time * 1000);
+  }
+
+  return function () {
+    lastCallTime = Date.now();
+    scheduleCall();
+  };
+}
+
+function logName() {
+}
+
+const lazyLogName = callOnceAfter(logName, 2);
+/////////////////////////////////////////////
+function limitCallForEveryDuration(func, durationInSeconds) {
+  let lastCallTime = 0;
+  let isScheduled = false;
+
+  function scheduleCall() {
+    isScheduled = true;
+    setTimeout(function () {
+      if (isScheduled) {
+        func();
+        lastCallTime = Date.now();
+        isScheduled = false;
+      }
+    }, durationInSeconds * 1000);
+  }
+
+  return function () {
+    const currentTime = Date.now();
+    const elapsedTime = currentTime - lastCallTime;
+
+    if (elapsedTime >= durationInSeconds * 1000) {
+      func();
+      lastCallTime = currentTime;
+    } else {
+      if (!isScheduled) {
+        scheduleCall();
+      }
+    }
+  };
+}
+
+function logName() {}
+
+const limitedLogName = limitCallForEveryDuration(logName, 0.5);
+
+for (let i = 0; i < 10000; i++) {
+  limitedLogName();
+}
+/////////////////////////////////////////////////////////
