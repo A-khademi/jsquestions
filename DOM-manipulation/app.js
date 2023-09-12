a0ac361bc3141600e0267e5cbe8b83c099ded099
Ali <118660343+A-khademi@users.noreply.github.com>
2023-09-12 13:50:45 +0330

    Update app.js

--- DOM-manipulation/app.js
+++ DOM-manipulation/app.js
@@ -72,6 +72,46 @@ function render(element) {
     const elementString = `<${name}${renderAttributes(attributes)}>${renderChildren(children)}</${name}>`;
     return elementString;
 }
+//////////////////////////////////////////////////////////////////////////////////////
+function generateForm(formInputs) {
+    const form = document.createElement("form");
+    form.setAttribute("action", "");
 
+    formInputs.forEach(input => {
+        const label = document.createElement("label");
+        label.setAttribute("for", input.id);
+        label.textContent = input.label;
+
+        if (input.Type === "text" || input.Type === "email" || input.Type === "number" || input.Type === "password") {
+            const inputField = document.createElement("input");
+            inputField.setAttribute("type", input.inputType || "text");
+            inputField.setAttribute("id", input.id);
+            inputField.setAttribute("placeholder", input.placeholder || "");
+            form.appendChild(label);
+            form.appendChild(inputField);
+        } else if (input.Type === "checkbox") {
+            const checkbox = document.createElement("input");
+            checkbox.setAttribute("type", "checkbox");
+            checkbox.setAttribute("id", input.id);
+            form.appendChild(checkbox);
+            form.appendChild(label);
+        } else if (input.Type === "select" && input.options && input.options.length > 0) {
+            const select = document.createElement("select");
+            select.setAttribute("id", input.id);
+
+            input.options.forEach(option => {
+                const optionElement = document.createElement("option");
+                optionElement.setAttribute("value", option.value);
+                optionElement.textContent = option.label;
+                select.appendChild(optionElement);
+            });
+
+            form.appendChild(label);
+            form.appendChild(select);
+        }
+    });
+
+    document.body.appendChild(form);
+}
 
 
